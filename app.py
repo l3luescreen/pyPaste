003774761781c54d00c13392843a1fec16629beb
Thanakan Boonma <KanZ@KanZ-MacBook-Air.local>
2021-07-20 02:52:37 +0700

    add feature to paste

--- app.py
+++ app.py
@@ -1,41 +1,42 @@
 import tkinter as tk
-from functools import partial  
+import clipboard
 
+from functools import partial
 class Application(tk.Frame):
     def __init__(self, master=None):
         super().__init__(master)
         self.master = master
         self.pack()
-        self.create_widgets()
-    
-    def copy_to_clipboard(self, text):
-        print("copy")
-        clipb = tk.Tk()
-        clipb.withdraw()
-        clipb.clipboard_clear()
-        clipb.clipboard_append(text.get())
-        clipb.update() # now it stays on the clipboard after the window is closed
-        clipb.destroy()
 
-    def create_widgets(self):
-        tk.Label(self, text="Set text").grid(row=0)
+        # variable
+        self.data_clip = tk.StringVar()
 
-        data_clip = tk.StringVar()
+        item_text = tk.Label(self, text="Set text")
 
-        text1 = tk.Entry(self, textvariable=data_clip)
+        text1 = tk.Entry(self, textvariable = self.data_clip)
 
-        copy_to_clipboard = partial(self.copy_to_clipboard, data_clip) #now copy_to_clipboard has 2 arguments
         
-        btn_copy = tk.Button(
-            self,
-            text="copy",
-            command=copy_to_clipboard
-        )
+        copy_to_clipboard = partial(self.copy_to_clipboard, self.data_clip) #now copy_to_clipboard has 2 arguments
+        btn_copy = tk.Button(self, text="copy", command=copy_to_clipboard)
+        btn_paste = tk.Button(self, text="paste", command=self.paste_text)
 
+
+        # btn_copy.bind("<Enter>", self.copy_to_clipboard(self.data_clip))
+
+        item_text.grid(row=0, column=0)
         text1.grid(row=0, column=1)
         btn_copy.grid(row=0, column=2)
+        btn_paste.grid(row=0, column=3)
     
+    def copy_to_clipboard(self, text):
+        data = text.get()
+        clipboard.copy(data)
+
+    def paste_text(self):
+        print(clipboard.paste())
+  
 
 root = tk.Tk()
+root.title("pyPaste")
 app = Application(master=root)
 app.mainloop()
